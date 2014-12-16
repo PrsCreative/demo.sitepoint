74b4f0dd70de72fbcb04ec9afb4f43e9f2352b11
Erchiragdodia <chirag.d7373@gmail.com>
2014-12-16 23:11:05 +0530

    initial commit

---
+++ app/code/local/Sitepoint/Articles/Block/Articles.php
@@ -0,0 +1,9 @@
+<?php
+class Sitepoint_Articles_Block_Articles extends Mage_Core_Block_Template
+{
+    public function myfunction()
+    {
+        
+        echo "Hello world";
+    }
+}
---
+++ app/code/local/Sitepoint/Articles/Model/Articles.php
@@ -0,0 +1,9 @@
+<?php
+class Sitepoint_Articles_Model_Articles extends Mage_Core_Model_Abstract
+{
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init('articles/articles');
+    }
+}
